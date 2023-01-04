5a7580c1e76d49f4dcfa62bd4e2fec4235392b81
kevinqhpham <kevin617.pham@gmail.com>
2023-01-03 19:56:27 -0600

    added functionality to backspace, clear, and plus/minus

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function add(num1, num2){
-    let first = parseInt(num1);
-    let second = parseInt(num2);
+    let first = parseFloat(num1);
+    let second = parseFloat(num2);
     return first+second;
 }
 
 function subtract(num1, num2){
-    let first = parseInt(num1);
-    let second = parseInt(num2);
+    let first = parseFloat(num1);
+    let second = parseFloat(num2);
     return first-second;
 }
 
 function multiply(num1, num2){
-    let first = parseInt(num1);
-    let second = parseInt(num2);
+    let first = parseFloat(num1);
+    let second = parseFloat(num2);
     return first*second;
 }
 
 function divide(num1, num2){
-    let first = parseInt(num1);
-    let second = parseInt(num2);
+    let first = parseFloat(num1);
+    let second = parseFloat(num2);
     return first/second;
 }
 function operate(op, num1, num2){
@@ -67,10 +67,9 @@ function identify(){ //finds which operator user inputs
     }
     return screenOp;
 } 
-
+const dis = document.querySelector(".display");
 function calculate(){ //produces answer when equal key is pressed
     const eq = document.querySelector("#equal");
-    const dis = document.querySelector(".display");
     eq.addEventListener('click', () => {
         let ind = trueDisplay.indexOf(identify());
         let num1 = trueDisplay.slice(0,ind);
@@ -81,5 +80,37 @@ function calculate(){ //produces answer when equal key is pressed
     });
 }
 
+function clear(){
+    const cl = document.querySelector("#c");
+    cl.addEventListener('click', () => {
+        trueDisplay = "";
+        dis.textContent = trueDisplay;
+    });
+}
+
+function backSpace(){
+    const back = document.querySelector("#back");
+    back.addEventListener('click', () => {
+        trueDisplay = trueDisplay.slice(0,trueDisplay.length-1);
+        dis.textContent = trueDisplay;
+    });
+}
+
+function changeSigns(){
+    const change = document.querySelector("#change");
+    change.addEventListener('click', () => {
+        if(trueDisplay.slice(0,1) == "-"){
+            trueDisplay = trueDisplay.slice(1);
+        }
+        else{
+            trueDisplay = "-" + trueDisplay.slice(0);
+        }
+        dis.textContent = trueDisplay;
+    });
+}
+
 display();
 calculate();
+clear();
+backSpace();
+changeSigns();
